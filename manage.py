969a12411987f1770ada7eba025e7f2111ec105e
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-06-15 18:13:48 +0800

    first

--- manage.py
+++ manage.py
@@ -4,10 +4,27 @@
 # @Email   : li.yan_li@neusoft.com
 # @File    : manage.py.py
 # @Software: PyCharm
-from  flask import Flask
+import redis
+from flask import Flask
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
 
+class Config(object):
+    """工程信息配置"""
+    DEBUG = True
+    # 导入数据库配置
+    # 设置数据库连接
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1:3306/information22'
+    # 动态追踪设置
+    app.config['SQLALCHEMY_TRACK_MODUFICATIONS'] = True
+    # 显示原始sql
+    app.config['SQLALCHEMY_ECHO'] = True
+
+    RDIES_HOST = "127.0.0.1"
+    RDIES_PORT = 6379
 @app.route('/')
 def index():
     return "index"
