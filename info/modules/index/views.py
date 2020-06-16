283674490e3b25579ff123108ddb1dcf84b1cd6b
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-06-16 19:26:27 +0800

    ico

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -7,10 +7,13 @@
 
 
 # 蓝图
-from flask import render_template
+from flask import render_template, current_app
 
 from . import index_blu
 @index_blu.route('/')
 def index():
     # return "index"
     return render_template('news/index.html')
+@index_blu.route('/favicon.ico')
+def get_web_logo():
+    return current_app.send_static_file('news/favicon.ico')
